f56b866d6c66bf9d3f73c6b6e96c6e9b5bdf790c
Rajo <rajo.timmermans@gmail.com>
2024-07-30 14:21:04 +0200

    UserRestController finished - story 6

---
+++ src/main/java/be/ucll/controller/UserRestController.java
@@ -0,0 +1,27 @@
+package be.ucll.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import be.ucll.model.User;
+import be.ucll.service.UserService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+    private UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+    
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
