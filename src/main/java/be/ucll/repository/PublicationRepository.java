15bab659c1e18091ba22c92ef67b1e2f090aa9a9
Yannick Van Ham <yannick.vanham@multimedi.be>
2024-08-05 10:35:30 +0200

    Fix formatting

--- src/main/java/be/ucll/repository/PublicationRepository.java
+++ src/main/java/be/ucll/repository/PublicationRepository.java
@@ -9,7 +9,6 @@ import be.ucll.model.Book;
 import be.ucll.model.Magazine;
 import be.ucll.model.Publication;
 
-
 @Repository
 public class PublicationRepository {
     public List<Publication> books;
@@ -17,26 +16,24 @@ public class PublicationRepository {
 
     public PublicationRepository() {
         books = new ArrayList<>(List.of(
-            new Book("Vikings", "Arthur", "9783161484100", 2010, 10),
-            new Book("Annabel", "James Barnes", "9783161484140", 2014, 10),
-            new Book("The Hunger Games", "Suzanne collins", "9783161488140", 2011, 10),
-            new Book("Franklin Brothers", "Josh Jefferson", "9783169484140", 2008, 10)
-            ));
-        
+                new Book("Vikings", "Arthur", "9783161484100", 2010, 10),
+                new Book("Annabel", "James Barnes", "9783161484140", 2014, 10),
+                new Book("The Hunger Games", "Suzanne collins", "9783161488140", 2011, 10),
+                new Book("Franklin Brothers", "Josh Jefferson", "9783169484140", 2008, 10)));
+
         magazines = new ArrayList<>(List.of(
-            new Magazine("The Guardian", "James Barnes", "12345678", 2010, 10),
-            new Magazine("The New York Times", "Suzanne Collins", "12345671", 2014, 10),
-            new Magazine("The Washington Post", "Josh Jefferson", "12345672", 2011, 10),
-            new Magazine("The Wall Street Journal", "Josh Jefferson", "12345673", 2008, 10)
-        ));
+                new Magazine("The Guardian", "James Barnes", "12345678", 2010, 10),
+                new Magazine("The New York Times", "Suzanne Collins", "12345671", 2014, 10),
+                new Magazine("The Washington Post", "Josh Jefferson", "12345672", 2011, 10),
+                new Magazine("The Wall Street Journal", "Josh Jefferson", "12345673", 2008, 10)));
     }
 
     public List<Publication> findByTitleAndType(String title, String type) {
         List<Publication> filteredByType = new ArrayList<>();
-        if(type == null || type.equalsIgnoreCase("Book")) {
+        if (type == null || type.equalsIgnoreCase("Book")) {
             filteredByType.addAll(books);
         }
-        if(type == null || type.equalsIgnoreCase("Magazine")) {
+        if (type == null || type.equalsIgnoreCase("Magazine")) {
             filteredByType.addAll(magazines);
         }
 
@@ -45,7 +42,7 @@ public class PublicationRepository {
         } else {
             List<Publication> filteredByTitle = new ArrayList<>();
             for (Publication publication : filteredByType) {
-                if(publication.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                if (publication.getTitle().toLowerCase().contains(title.toLowerCase())) {
                     filteredByTitle.add(publication);
                 }
             }
@@ -56,16 +53,16 @@ public class PublicationRepository {
     public List<Publication> filterByAvailableCopies(int availableCopies) {
         List<Publication> filteredByAvailableCopies = new ArrayList<>();
         for (Publication publication : books) {
-            if(publication.getAvailableCopies() >= availableCopies){
+            if (publication.getAvailableCopies() >= availableCopies) {
                 filteredByAvailableCopies.add(publication);
-                }
-           }
-        for (Publication publication : magazines) {
-            if(publication.getAvailableCopies() >= availableCopies){
-                filteredByAvailableCopies.add(publication);
-                }
             }
-           return filteredByAvailableCopies;
         }
+        for (Publication publication : magazines) {
+            if (publication.getAvailableCopies() >= availableCopies) {
+                filteredByAvailableCopies.add(publication);
+            }
+        }
+        return filteredByAvailableCopies;
+    }
 
 }
