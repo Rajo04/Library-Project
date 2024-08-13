72331408b584ea639656a6d616f969804ba5fac0
Yannick Van Ham <yannick.vanham@multimedi.be>
2024-08-13 10:38:42 +0200

    Add feedback

--- src/test/java/be/ucll/integration/UserIntegrationTest.java
+++ src/test/java/be/ucll/integration/UserIntegrationTest.java
@@ -3,14 +3,18 @@ package be.ucll.integration;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import be.ucll.model.User;
 import be.ucll.repository.UserRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -23,11 +27,27 @@ public class UserIntegrationTest {
         @Autowired
         private UserRepository userRepository;
 
-        @AfterEach
-        public void resetData() {
-                userRepository.resetRepositoryData();
+
+        // Note: userRepository.resetRepositoryData() ❌ this is NOT a concern of the user repository, only of the test logic, this method should be in the test perhaps as a private helper method (see below),
+        //                      OR use @DirtiesContext (on class or on test) to tell Spring Boot Test to reset the spring container (this is slow/expensive)
+        // @AfterEach
+        // public void resetData() {
+        //         userRepository.resetRepositoryData();
+        // }
+
+        // @AfterEach
+        private void resetRepositoryData(){
+                userRepository.users = new ArrayList<>(List.of(
+                new User("John Doe", 56, "john.doe@ucll.be", "john1234"),
+                new User("Jane Toe", 30, "jane.toe@ucll.be", "jane1234"),
+                new User("Jack Doe", 5, "jack.doe@ucll.be", "jack1234"),
+                new User("Sarah Doe", 4, "sarah.doe@ucll.be", "sarah1234")));
         }
 
+        // TODO: Handvol onnodige velden (zoals startDateFirstLoanOfJohn,
+        // endDateFirstLoanOfJohn), deze horen eerder in een test methode waar ze
+        // gebruikt worden, tenzij ze enorm vaak hergebruikt worden, in which case make
+        // sure you reset these in @BeforeEach)
         String startDateFirstLoanOfJohn = LocalDate.now().minusDays(2).toString();
         String endDateFirstLoanOfJohn = LocalDate.now().minusDays(1).toString();
 
@@ -35,6 +55,8 @@ public class UserIntegrationTest {
 
         @Test
         public void givenUsers_whenGetUsers_thenUsersAreReturned() {
+                // TODO: Use text blocks (triple quotes) instead of many mini-strings and
+                // concatenation (+) and \n and \"
                 webTestClient.get().uri("/users").exchange().expectStatus().isOk().expectBody().json(
                                 "[\n" +
                                                 "  {\n" +
@@ -117,6 +139,11 @@ public class UserIntegrationTest {
 
         @Test
         public void givenUserEmailOfJohnDoe_whenGetLoansByEmail_thenCorrectLoansForEmailAreReturned() {
+                // TODO: Voor al deze tests, maak gewoonte om van chained-methoden nieuwe regels
+                // te maken waar logisch:
+                // Voorbeeld: webTestClient.get().uri("/users/age/0/101").exchange()
+                // .expectStatus().isOk()
+                // .expectBody().json(
                 webTestClient.get().uri("/users/john.doe@ucll.be/loans").exchange().expectStatus().isOk().expectBody()
                                 .json(
                                                 "[\n" +
@@ -166,9 +193,12 @@ public class UserIntegrationTest {
                                                                 "]");
         }
 
-        @Test
+        @Test // TODO: Wees consistent in naamgeving (elders heb je dat creatingUser genoemd,
+              // doe dat hier ook)
+        @DirtiesContext
         public void givenNoUserWithEmailFromPeterGriffin_whenPostingNewUser_thenNewUserIsPosted() {
-                webTestClient.post().uri("/users").header("Content-Type", "application/json")
+                webTestClient.post().uri("/users")
+                                .header("Content-Type", "application/json")
                                 .bodyValue(
                                                 "{\n" +
                                                                 "  \"name\": \"Peter Griffin\",\n" +
@@ -177,10 +207,8 @@ public class UserIntegrationTest {
                                                                 "  \"password\": \"peter1234\"\n" +
                                                                 "}")
                                 .exchange()
-                                .expectStatus()
-                                .isOk()
-                                .expectBody()
-                                .json(
+                                .expectStatus().isOk()
+                                .expectBody().json(
                                                 "{\n" +
                                                                 "    \"name\": \"Peter Griffin\",\n" +
                                                                 "    \"age\": 22,\n" +
@@ -192,6 +220,7 @@ public class UserIntegrationTest {
         }
 
         @Test
+        @DirtiesContext
         public void givenUserJohnDoe_whenChangingUserInfoByEmail_thenUserInfoIsExceptForEmailIsChanged() {
                 webTestClient.put().uri("/users/john.doe@ucll.be").header("Content-Type", "application/json")
                                 .bodyValue(
@@ -216,11 +245,12 @@ public class UserIntegrationTest {
         }
 
         @Test
+        @DirtiesContext
         public void givenUserJaneToe_whenDeletingUserByEmail_thenUserIsCorrectlyDeleted() {
                 webTestClient.delete().uri("/users/jane.toe@ucll.be").exchange().expectStatus().isOk()
                                 .expectBody(String.class)
                                 .isEqualTo("User successfully deleted.");
-
+                // TODO: assertTrue(!foo) --> assertFalse(foo)
                 assertTrue(!userRepository.userExists("jane.toe@ucll.be"));
         }
 }
